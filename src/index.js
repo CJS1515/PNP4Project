b8b2871dc67d439a3796d7c1a5ad3dbc34befb61
Cobi Scherb <cjscherb@student.fullsail.edu>
2023-11-23 15:17:48 -0500

    Week 1 Push 1, starting on functionality

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+//import ReactDOM from "react-dom";
 import "jquery";
 import "popper.js/dist/umd/popper";
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
-import NavBar from "./App";
+import App from "./App";
 import "./index.css";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(<NavBar />, document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
+
+//ReactDOM.render(<NavBar />, document.getElementById("root"));
